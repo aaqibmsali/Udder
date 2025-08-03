0a5a9eaed612cae81c422e77725a4eac6d0a9c55
Aaqib <aaqib.m.sali@gmail.com>
2025-08-03 13:12:14 +0530

    Initial Commit

---
+++ UdderTests/UdderTests.swift
@@ -0,0 +1,17 @@
+//
+//  UdderTests.swift
+//  UdderTests
+//
+//  Created by Aaqib Mohammed Sali on 03/08/25.
+//
+
+import Testing
+@testable import Udder
+
+struct UdderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
